d9159c404fc6a649d5ed7b373d178a1c447e3dd6
qawbecrdtey <qawbecrdtey@naver.com>
2018-07-01 04:08:58 +0900

    clear main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include "include/math/Matrix.h"
-using namespace dvdeep::math;
-double func(double x)
-{
-    return x * (1 - x);
-}
+#include "include/network/FFN.h"
+
+using namespace dvdeep::network;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    double a[6]={1,2,3,4,5,6};
-    double b[9]={1,2,3,4,5,6,7,8,9};
-    Matrix A=Matrix(2,3,a);
-    Matrix B=Matrix(3,3,b);
-    std::cout<<A.transpose()<<std::endl;
-    std::cout<<A*B<<std::endl;
-    std::cout<<A.unaryExpr(func)<<std::endl;
-    return 0;
+    FFN net;
+
 }
